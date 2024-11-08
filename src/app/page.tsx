d8558fec6adac3c1f1bde23924a2730aa63b351f
Emil Jacobsson <emil@gnaget.se>
2024-11-08 05:38:29 +0100

    Update homepage layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,66 @@
 "use client";
 import FeaturedCarousel from "@/components/home/featured-carousel";
-
 import React from "react";
-
-// import function to register Swiper custom elements
 import { register } from "swiper/element/bundle";
 import CategorySelector from "@/components/home/category-selector";
 import NewsletterSignup from "@/components/newsletter-signup";
 import CardsSwiper from "@/components/home/cards-swiper";
-// register Swiper custom elements
+
 register();
 
 export default function Home() {
   return (
     <>
-      <div className="max-w-[1400px] mx-auto px-4 sm:px-16 mt-0 sm:mt-12 mb-24 ">
-        {/* Featured Recipes Intro Section */}
-        <section className="mt-8 sm:mt-12 mb-6 sm:mb-8">
-          <h2 className="text-2xl font-semibold tracking-tight">
-            Featured Recipes
-          </h2>
-          <div className="mt-2 w-16 h-1 bg-gradient-to-r from-green-400 via-green-600 to-green-400 rounded-full"></div>
-        </section>
+      {/* Featured Recipes Section */}
+      <div className="w-full  py-12">
+        <div className="max-w-[1400px] mx-auto px-4 sm:px-16">
+          <section className="mb-6 sm:mb-8">
+            <h2 className="text-2xl font-semibold tracking-tight">
+              Featured Recipes
+            </h2>
+            <div className="mt-2 w-16 h-1 bg-gradient-to-r from-green-400 via-green-600 to-green-400 rounded-full"></div>
+          </section>
+          <FeaturedCarousel />
+        </div>
+      </div>
 
-        {/* Carousel Header */}
-        <FeaturedCarousel />
+      {/* Browse by Category Section */}
+      <div className="w-full  py-12">
+        <div className="max-w-[1400px] mx-auto px-4 sm:px-16">
+          <section className="mb-6 sm:mb-8">
+            <h2 className="text-2xl font-semibold tracking-tight">
+              Browse by Category
+            </h2>
+            <div className="mt-2 w-16 h-1 bg-gradient-to-r from-green-400 via-green-600 to-green-400 rounded-full"></div>
+          </section>
+          <CategorySelector />
+        </div>
+      </div>
 
-        {/* Browse by Category Intro Section */}
-        {/*// TODO: Replace with search section as category selection will be handled in navbar */}
-        <section className="mt-16 sm:mt-24 mb-6 sm:mb-8">
-          <h2 className="text-2xl font-semibold tracking-tight">
-            Browse by Category
-          </h2>
-          <div className="mt-2 w-16 h-1 bg-gradient-to-r from-green-400 via-green-600 to-green-400 rounded-full"></div>
-        </section>
-        <CategorySelector />
+      {/* October Recipes Section */}
+      <div className="w-full  py-12">
+        <div className="max-w-[1400px] mx-auto px-4 sm:px-16">
+          <section className="mb-6 sm:mb-8">
+            <h2 className="text-2xl font-semibold tracking-tight">
+              October Recipes
+            </h2>
+            <div className="mt-2 w-16 h-1 bg-gradient-to-r from-green-400 via-green-600 to-green-400 rounded-full"></div>
+          </section>
+          <CardsSwiper category={"Popular Recipes"} />
+        </div>
+      </div>
 
-        {/*CardsSwiper Intro Section */}
-        <section className="mt-16 sm:mt-24 mb-6 sm:mb-8">
-          <h2 className="text-2xl font-semibold tracking-tight">
-            October Recipes
-          </h2>
-          <div className="mt-2 w-16 h-1 bg-gradient-to-r from-green-400 via-green-600 to-green-400 rounded-full"></div>
-        </section>
-        <CardsSwiper category={"Popular Recipes"} />
-
-        {/* Newsletter Intro Section */}
-        <section className="mt-16 sm:mt-16 mb-6 sm:mb-8">
-          <h2 className="text-2xl font-semibold tracking-tight">
-            Get Weekly Recipe Inspiration
-          </h2>
-          <div className="mt-2 w-16 h-1 bg-gradient-to-r from-green-400 via-green-600 to-green-400 rounded-full"></div>
-        </section>
-        <NewsletterSignup />
+      {/* Newsletter Signup Section */}
+      <div className="w-full py-12">
+        <div className="max-w-[1400px] mx-auto px-4 sm:px-16">
+          <section className="mb-6 sm:mb-8">
+            <h2 className="text-2xl font-semibold tracking-tight">
+              Get Weekly Recipe Inspiration
+            </h2>
+            <div className="mt-2 w-16 h-1 bg-gradient-to-r from-green-400 via-green-600 to-green-400 rounded-full"></div>
+          </section>
+          <NewsletterSignup />
+        </div>
       </div>
     </>
   );
