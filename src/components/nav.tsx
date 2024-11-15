b0cf6ad12a53a832a25eb60c7dc2a7125fcce417
Emil Jacobsson <emil@gnaget.se>
2024-11-15 01:37:59 +0100

    Update web navbar.

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,7 +3,8 @@
 import * as React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Menu } from "lucide-react";
+import { LogIn, Menu, Play, UserPlus } from "lucide-react";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -42,122 +43,148 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = React.useState(false);
   const pathname = usePathname();
 
-  // Helper function to determine if a link is active
   const isActiveLink = (href: string) => {
-    if (href === "/") {
-      return pathname === "/";
-    } else if (href.startsWith("/category/")) {
-      return pathname.startsWith("/category/");
-    } else {
-      return pathname === href;
-    }
+    if (href === "/") return pathname === "/";
+    if (href.startsWith("/category/")) return pathname.startsWith("/category/");
+    return pathname === href;
   };
 
   return (
-    <nav className="">
-      <div className="mx-auto px-4 sm:px-6 lg:px-12">
-        <div className="relative flex items-center justify-between h-16">
-          {/* Logo */}
-          <Link href="/" className="flex-shrink-0 flex gap-2">
-            <h1 className="font-semibold">
-              Recept <span className="text-green-600">Radar</span>
-            </h1>
-          </Link>
+    <>
+      {/* Desktop Navbar */}
+      <nav
+        className={`absolute top-0 left-0 w-full z-50 hidden lg:block ${
+          pathname === "/" ? "dark" : ""
+        }`}
+      >
+        <div className="mx-auto px-4 sm:px-6 lg:px-12">
+          <div className="relative flex items-center justify-between h-16">
+            {/* Logo */}
+            <Link href="/" className="flex-shrink-0 flex gap-2">
+              <h1 className="font-semibold text-primary">
+                Recept <span className="text-green-600">Radar</span>
+              </h1>
+            </Link>
 
-          {/* Desktop Navigation Links */}
-          <div className="hidden lg:block lg:absolute lg:left-1/2 lg:-translate-x-1/2 lg:flex lg:items-center lg:justify-center">
-            <div className="flex items-baseline space-x-4">
-              <Link
-                href="/"
-                className={`py-2 text-sm font-medium whitespace-nowrap hover:text-primary ${
-                  isActiveLink("/") ? "text-primary" : "text-muted-foreground"
-                }`}
-              >
-                Home
-              </Link>
-              <Link
-                href="/recipes"
-                className={`px-3 py-2 rounded-md text-sm font-medium whitespace-nowrap truncate hover:text-primary ${
-                  isActiveLink("/recipes")
-                    ? "text-primary"
-                    : "text-muted-foreground"
-                }`}
-              >
-                Recipes
-              </Link>
+            {/* Desktop Navigation Links */}
+            <div className="flex items-center justify-center absolute left-1/2 transform -translate-x-1/2">
+              <div className="flex items-baseline space-x-8">
+                <Link
+                  href="/"
+                  className={`py-2 text-sm font-medium whitespace-nowrap hover:text-primary ${
+                    isActiveLink("/") ? "text-primary" : "text-muted-foreground"
+                  }`}
+                >
+                  Home
+                </Link>
+                <Link
+                  href="/recipes"
+                  className={`px-3 py-2 rounded-md text-sm font-medium whitespace-nowrap truncate hover:text-primary ${
+                    isActiveLink("/recipes")
+                      ? "text-primary"
+                      : "text-muted-foreground"
+                  }`}
+                >
+                  Recipes
+                </Link>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <a
+                      className={`hover:cursor-pointer px-3 py-2 rounded-md text-sm font-medium whitespace-nowrap truncate hover:text-primary ${
+                        pathname.startsWith("/category/")
+                          ? "text-primary"
+                          : "text-muted-foreground"
+                      }`}
+                    >
+                      Categories
+                    </a>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-screen left-0 mt-2">
+                    <div className="grid grid-cols-4 lg:grid-cols-5 gap-4 p-4">
+                      {categories.map((category) => {
+                        const categoryHref = `/category/${category.toLowerCase()}`;
+                        const isActive = pathname === categoryHref;
+                        return (
+                          <DropdownMenuItem key={category} asChild>
+                            <Link
+                              href={categoryHref}
+                              className={`${
+                                isActive
+                                  ? "text-primary"
+                                  : "text-muted-foreground"
+                              }`}
+                            >
+                              {category}
+                            </Link>
+                          </DropdownMenuItem>
+                        );
+                      })}
+                    </div>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <Link
+                  href="/upload"
+                  className={`px-3 py-2 rounded-md text-sm font-medium whitespace-nowrap truncate hover:text-primary ${
+                    isActiveLink("/upload")
+                      ? "text-primary"
+                      : "text-muted-foreground"
+                  }`}
+                >
+                  Upload a Recipe
+                </Link>
+                <Link
+                  href="/my-recipes"
+                  className={`px-3 py-2 rounded-md text-sm font-medium whitespace-nowrap truncate hover:text-primary ${
+                    isActiveLink("/my-recipes")
+                      ? "text-primary"
+                      : "text-muted-foreground"
+                  }`}
+                >
+                  My Recipes
+                </Link>
+              </div>
+            </div>
 
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <a
-                    className={`hover:cursor-pointer px-3 py-2 rounded-md text-sm font-medium whitespace-nowrap truncate hover:text-primary ${
-                      pathname.startsWith("/category/")
-                        ? "text-primary"
-                        : "text-muted-foreground"
-                    }`}
-                  >
-                    Categories
-                  </a>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-screen left-0 mt-2">
-                  <div className="grid grid-cols-4 lg:grid-cols-5 gap-4 p-4">
-                    {categories.map((category) => {
-                      const categoryHref = `/category/${category.toLowerCase()}`;
-                      const isActive = pathname === categoryHref;
-                      return (
-                        <DropdownMenuItem key={category} asChild>
-                          <Link
-                            href={categoryHref}
-                            className={`${
-                              isActive
-                                ? "text-primary"
-                                : "text-muted-foreground"
-                            }`}
-                          >
-                            {category}
-                          </Link>
-                        </DropdownMenuItem>
-                      );
-                    })}
-                  </div>
-                </DropdownMenuContent>
-              </DropdownMenu>
-              <Link
-                href="/upload"
-                className={`px-3 py-2 rounded-md text-sm font-medium whitespace-nowrap truncate hover:text-primary ${
-                  isActiveLink("/upload")
-                    ? "text-primary"
-                    : "text-muted-foreground"
-                }`}
-              >
-                Upload a Recipe
-              </Link>
-              <Link
-                href="/my-recipes"
-                className={`px-3 py-2 rounded-md text-sm font-medium whitespace-nowrap truncate hover:text-primary ${
-                  isActiveLink("/my-recipes")
-                    ? "text-primary"
-                    : "text-muted-foreground"
-                }`}
-              >
-                My Recipes
-              </Link>
+            {/* Desktop Sign-in Buttons */}
+            <div className="flex items-center gap-2 text-primary ">
+              <Button variant="link" size="sm">
+                <LogIn
+                  className="mr-2
+                
+                
+                "
+                  size={18}
+                />
+                Sign in
+              </Button>
+              <Button variant={"link"} size="sm">
+                <UserPlus className="mr-2" size={18} />
+                Get Started
+              </Button>
             </div>
           </div>
+        </div>
+      </nav>
 
-          {/* Sign-in Buttons */}
-          <div className="hidden lg:flex items-center gap-2">
-            <Button variant="outline" size="sm">
-              Sign in
-            </Button>
-            <Button size="sm">Get Started</Button>
-            <ModeToggle />
-          </div>
+      {/* Mobile Navbar */}
+      <nav
+        className={`fixed top-0 left-0 w-full z-50 lg:hidden ${
+          pathname === "/" ? "dark" : ""
+        }`}
+      >
+        <div className="mx-auto px-4 sm:px-6 lg:px-12">
+          <div className="relative flex items-center justify-between h-16">
+            {/* Logo */}
+            <Link href="/" className="flex-shrink-0 flex gap-2">
+              <h1 className="font-semibold text-primary">
+                Recept <span className="text-green-600">Radar</span>
+              </h1>
+            </Link>
 
-          {/* Mobile Menu Button */}
-          <div className="lg:hidden">
+            {/* Mobile Navigation */}
             <Sheet open={isOpen} onOpenChange={setIsOpen}>
               <SheetTrigger asChild>
-                <Button variant="ghost" size="icon">
+                <Button variant="ghost" className="text-primary" size="icon">
                   <Menu className="h-6 w-6" />
                   <span className="sr-only">Open menu</span>
                 </Button>
@@ -179,7 +206,7 @@ export default function Navbar() {
                     <Link
                       href="/recipes"
                       className={`px-3 py-2 rounded-md text-sm font-medium ${
-                        isActiveLink("/Recipes")
+                        isActiveLink("/recipes")
                           ? "text-primary"
                           : "text-muted-foreground"
                       }`}
@@ -265,7 +292,7 @@ export default function Navbar() {
             </Sheet>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
